9a24b6ec7b309daf09e46462fbcd837a89129e34
Azikenp <azikenp26@gmail.com>
2023-08-28 22:27:58 +0100

    additioms

--- src/components/bookList/BookList.jsx
+++ src/components/bookList/BookList.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { useGlobalContext } from '../../Context'
+import Loader from '../loader/Loader'
+import coverImg from '../../images/cover_not_found.jpg'
+import './BookList.css'
+
+//https://covers.openlibrary.org/b/id/240727-S.jpg
 
 function BookList() {
+  const {books, loading, resultTitle} = useGlobalContext();
+  const booksWithCovers = books.map((singlebook) => {
+    return{
+      ...singlebook,
+      //removing /works/ to get only id
+    }
+  })
+
+
+
   return (
-    <div>BookList</div>
+    
   )
 }
 
