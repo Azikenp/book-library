26c784e528b832b9b5cef9bdd4fc3705dac10158
Azikenp <azikenp26@gmail.com>
2023-08-29 10:59:53 +0100

    remoing dummy text from about section

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './About.css'
-import aboutImg from '../../images/about-img.jpg'
+import React from "react";
+import "./About.css";
+import aboutImg from "../../images/about-img.jpg";
 
 function About() {
   return (
@@ -15,18 +15,20 @@ function About() {
             <img src={aboutImg} alt="about image" />
           </div>
 
-
           <div className="about-text">
-            <h2 className="about-title fs-26 ls-1">
-               About Bookhub
-            </h2>
-            <p className="fs-17">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis minima consequuntur laudantium officia dolorem corporis perferendis obcaecati, ipsum nisi. Accusamus harum corporis vitae possimus ullam voluptatibus distinctio nisi adipisci, fugiat quaerat tenetur pariatur maxime assumenda blanditiis quidem cum illum, doloribus et id officiis nulla voluptas! Laboriosam facilis eos officiis eius.</p>
-            <p className="fs-17">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis corporis dolor consequuntur. Illum numquam commodi ut. Cum fuga molestias, assumenda sunt aspernatur nemo repudiandae voluptatum eum, delectus, accusantium similique minima!</p>
+            <h2 className="about-title fs-26 ls-1">About Bookhub</h2>
+            <p className="fs-17">
+            Welcome to our innovative and comprehensive Book Search App, your ultimate destination for exploring the literary world like never before. In a society increasingly driven by digital advancements, our app stands as a testament to the seamless fusion of technology and the written word. Whether you're an avid reader, a student, a researcher, or simply someone with a curiosity for knowledge, our app is designed to be your trusted companion on your literary journey.
+            </p>
+            <p className="fs-17">
+            Our app boasts a vast collection of titles, from timeless classics to contemporary bestsellers. Whether you're searching for a rare edition or the latest release, we have you covered.
+            <br />  Say goodbye to the frustration of aimless searching. Our app learns from your reading history and preferences, offering recommendations that align with your tastes. Discover new authors, genres, and hidden gems that you might have missed otherwise.
+            </p>
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
